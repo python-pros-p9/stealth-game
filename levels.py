716d98c8659bdbec3d7f0f51d9a2d2eed79516cb
Will <58743949+GreggsOfficial@users.noreply.github.com>
2020-04-06 21:49:51 +0100

    Update levels.py

--- levels.py
+++ levels.py
@@ -4,7 +4,6 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 from player import Player
-from interactions import Interaction
 #from playerInteraction import PlayerInteraction
 import walls
 #from projectileCollision import ProjectileCollision
